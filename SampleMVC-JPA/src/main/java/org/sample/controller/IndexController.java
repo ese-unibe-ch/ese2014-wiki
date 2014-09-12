44a352a0660b842c4dd04c1c15efd6f0b2b2c10f
Andrea Caracciolo <caracciolo@iam.unibe.ch>
2014-09-12 17:29:23 +0200

    updated readme

--- SampleMVC-JPA/src/main/java/org/sample/controller/IndexController.java
+++ SampleMVC-JPA/src/main/java/org/sample/controller/IndexController.java
@@ -5,7 +5,6 @@ import org.sample.controller.service.SampleService;
 import org.sample.exceptions.InvalidUserException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
